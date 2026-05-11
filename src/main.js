927517577bc24509fa847f14f23ee02a3e14cdd4
AimanDanishh <danishekhsan@gmail.com>
2026-05-11 10:56:07 +0800

    Task 4 - register router

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+
+import router from './router'
+
 import './assets/style.css'
 
-createApp(App).mount('#app')
+createApp(App)
+    .use(router)        // register router
+    .mount('#app')
